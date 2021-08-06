673090c783919568747c3f97891f046e34da0abb
Hamza Hamza <hamzaerrifai@hotmail.com>
2021-08-06 15:45:57 +0100

    ui changes

--- src/components/ColorCodes.js
+++ src/components/ColorCodes.js
@@ -6,28 +6,47 @@ import { useParams } from "react-router-dom";
  * @param {String} strColors
  * @returns {Array}
  */
-const parseColors = (strColors) => {
+const parseBgColors = (strColors) => {
   let colors = [];
   colors = strColors.split("-");
   colors = colors.map((elm) => "#" + elm);
   return colors;
 };
 
+/**
+ * returns white or black de pending on the passed color
+ * @param {String} color
+ * @returns {String}
+ */
+const getColor = (color) => {
+  if (color.length === 7) {
+    const r = "0x" + color[1] + color[2];
+    const g = "0x" + color[3] + color[4];
+    const b = "0x" + color[5] + color[6];
+
+    if (r * 0.299 + g * 0.587 + b * 0.114 > 186) return "#000000";
+    return "#ffffff";
+  }
+  return "#000000";
+};
+
 function ColorCodes() {
   let { colors } = useParams();
-  let parsedColors = parseColors(colors);
-  
+  let parsedColors = parseBgColors(colors);
 
-  return parsedColors.map((elm, key) => (
-    <div
-      className="d-sm-block d-lg-inline text-center p-4"
-      key={key}
-      style={{ backgroundColor: elm }}
-      
-    >
-      <span className="colorCode">{elm}</span>
+  return (
+    <div className="d-inline-sm-flex d-md-flex">
+      {parsedColors.map((elm, key) => (
+        <div
+          className="text-center p-2"
+          key={key}
+          style={{ backgroundColor: elm, color: getColor(elm) }}
+        >
+          <span className="colorCode">{elm}</span>
+        </div>
+      ))}
     </div>
-  ));
+  );
 }
 
 export default ColorCodes;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,13 +2,18 @@ import React from "react";
 
 function NavBar() {
   return (
-    <nav className="navbar navbar-light bg-white shadow-sm">
-      <div className="container-fluid d-flex justify-content-center">
-        <a className="navbar-brand" href="#" id="logo">
-          ColorGen
-        </a>
+    <>
+      <nav className="navbar navbar-light bg-white shadow-sm">
+        <div className="container-fluid d-flex justify-content-center">
+          <a className="navbar-brand" href="#" id="logo">
+            ColorGen
+          </a>
+        </div>
+      </nav>
+      <div className="sub-nav">
+        <button className="btn border rounded shadow-sm">Generate</button>
       </div>
-    </nav>
+    </>
   );
 }
 
